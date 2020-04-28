63d73ce7b096afd1c925ef1fffca20393f4f8f36
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-28 20:29:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,9 @@ def reverse_each_word(string)
   my_array = string.split(/ /)
 
 
-  my_array.each{|element| element.reverse}
+  # my_array.each{|element| element.reverse}
   my_array.join(" ")
+
+
   return my_array
 end
