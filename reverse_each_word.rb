33e5bb055f35f72c5435503d7d8489dfb05c7adf
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-28 20:43:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,18 @@
 
 def reverse_each_word(sentence)
-  # sentence = sentence.split(/ /)
-  #
-  # sentence.each {|x| x.reverse }
-  #
-  # sentence.join(" ")
 
-  sentence.each {|x| x.reverse}
+  sentence.each do |x|
+    puts x.reverse 
+
+  end
 
 
 end
+
+
+
+# sentence = sentence.split(/ /)
+#
+# sentence.each {|x| x.reverse }
+#
+# sentence.join(" ")
