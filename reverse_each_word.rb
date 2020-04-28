f427f36d9f8a2d680c5d5383af31be9b2d696f27
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-28 21:01:17 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+
+def reverse_each_word(sentence)
+
+  new_array = sentence.split(/ /)
+
+  final_array = new_array.collect do |x|
+    x.reverse
+  end
+
+  final_array.join(" ")
+
+end
+
+
+
+# sentence = sentence.split(/ /)
+#
+# sentence.each {|x| x.reverse }
+#
+# sentence.join(" ")
