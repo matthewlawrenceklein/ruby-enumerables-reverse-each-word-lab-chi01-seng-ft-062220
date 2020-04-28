00324f2bfd4de8d800e21979c8b817b7b52fa2ad
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-28 20:40:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,12 @@
 
 def reverse_each_word(sentence)
-  sentence = sentence.split(/ /)
+  # sentence = sentence.split(/ /)
+  #
+  # sentence.each {|x| x.reverse }
+  #
+  # sentence.join(" ")
 
-  sentence.each {|x| x.reverse }
-
-  sentence.join(" ")
+  sentence.collect {|x| x.reverse}
 
 
 end
